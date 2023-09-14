19a068c2de83a2e5919b7baa35de701e246d79f4
Luisbert Aquino <luisbert_aquino@hotmail.com>
2023-09-14 14:55:21 -0400

    library.book model fields updated

--- library_app/models/library_book.py
+++ library_app/models/library_book.py
@@ -6,16 +6,37 @@ from odoo.exceptions import ValidationError
 class Book(models.Model):
     _name = "library.book"
     _description = "Book"
-    _order = "name, date_published desc"
-    _rec_name = "name"
-    _table = "library_book"
-    _log_access = True
-    _auto = True
+    
+    # String fields
     name = fields.Char("Title", required=True)
     isbn = fields.Char("ISBN")
-    active = fields.Boolean("Active?", default=True)
+    book_type = fields.Selection(
+        [("paper", "Paperback"),
+         ("hard", "Hardcover"),
+         ("electronic", "Electronic"),
+         ("other", "Other")], "Type")
+    notes = fields.Text("Internal Notes")
+    descr = fields.Html("Description")
+
+    # Numeric fields
+    copies = fields.Integer(default=1)
+    avg_rating = fields.Float("Average Rating", (3, 2))
+    price =  fields.Monetary("Price", "currency_id")
+
+    # Price helper 
+    currency_id = fields.Many2one("res.currency")
+
+    # Date and time fields:
     date_published = fields.Date()
+    last_borrow_date = fields.Datetime(
+        "Last Borrowed On",
+        default=lambda self: fields.Datetime.now())
+    
+    # Other fields
+    active = fields.Boolean("Active?", default=True)
     image = fields.Binary("Cover")
+    
+    # Relational Fields    
     publisher_id = fields.Many2one("res.partner", string="Publisher")
     author_ids = fields.Many2many("res.partner", string="Authors")
 
